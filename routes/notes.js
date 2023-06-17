13a4d6dca38dd468f0a8a145f2a44b2f1230651d
Matthew Bull <mattbull33@gmail.com>
2023-06-17 11:24:20 -0400

    set up format to start notes.js routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,22 @@
 const notes = require('express').Router();
+const uuid = require('../helpers/uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.get('/:id', (req, res) => {
+
+})
+
+notes.post('/', (req, res) => {
+
+})
+
+// bonus if time 
+//notes.delete('/:id', (req, res) => {
+
+// })
 
 module.exports = tips;
